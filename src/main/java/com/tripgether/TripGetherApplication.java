e2862504cd55fe869b2c018e11d99c4b7864fe31
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-10-03 20:38:22 +0900

    프로젝트 기본 설정 : feat : 기본 spring init https://github.com/Cassiiopeia/TripGether-BE/issues/1

---
+++ src/main/java/com/tripgether/TripGetherApplication.java
@@ -0,0 +1,13 @@
+package com.tripgether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripGetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripGetherApplication.class, args);
+	}
+
+}
